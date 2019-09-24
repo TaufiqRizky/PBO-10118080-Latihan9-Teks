a2dbbcca0d2c67ecb77ad02848ed65315a686a60
Taufiq R <taufiq.it2@gmail.com>
2019-09-24 20:35:51 +0700

    first commit

---
+++ src/pbo/pkg10118080/latihan9/teks/PBO10118080Latihan9Teks.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg10118080.latihan9.teks;
+
+/**
+ *
+ * @author 
+ * Nama :Taufiq Rizky
+ * NIM  :10118080
+ * Deskripsi Program    : program menampilkan jenis-jenis tipe teks
+ */
+public class PBO10118080Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab ='\t';
+        char newline='\n';
+        c='A';
+        System.out.println("Hasil"+newline+"Char :"+tab+"berisi "+c);
+    }
+    
+}
